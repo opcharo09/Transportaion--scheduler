b8902a47b57ed292ee0e2175e8e7772a08427138
unknown <oscar.charo09@gmail.com>
2019-04-07 21:37:20 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+config = {
+    apiKey: "AIzaSyAnrtk1Ul8ZQyvG1eulo7liKIUAEFeC7Lk",
+    authDomain: "my-codedb-project.firebaseapp.com",
+    databaseURL: "https://my-codedb-project.firebaseio.com",
+    projectId: "my-codedb-project",
+    storageBucket: "my-codedb-project.appspot.com",
+    messagingSenderId: "52035722347"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+   
